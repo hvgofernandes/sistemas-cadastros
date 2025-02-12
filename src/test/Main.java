0dcf755af7450c0b49725ab6ebe8e261c14483d0
hvgofernandes <hvgofernandes@gmail.com>
2025-02-12 18:36:23 -0300

    code/docs: conclusão dos 7 passos e atualização da changelog.

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,47 +1,33 @@
 package test;
 
 import entities.User;
-import java.util.Locale;
+
 import java.io.*;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         String path = "E:\\Documents\\diasante\\ws-intellij\\desafio\\sistemas-cadastros\\src\\archives";
-        String path2 = "E:\\Documents\\diasante\\ws-intellij\\desafio\\sistemas-cadastros\\src\\util\\archives\\mainMenuQuestions.txt";
 
-        List<String> perguntas = lerPerguntas(path + "\\formularioExemplo.txt");
+        List<String> perguntas = lerPerguntas(path + "\\mainMenuQuestions.txt");
         perguntas.forEach(System.out::println);
 
-        User user1 = coletarDados(sc);
-        salvarDados(user1, path);
-
-        System.out.println("#===#MENU PRINCIPAL#===#");
-        System.out.println("1 - Cadastrar usuário");
-        System.out.println("2 - Listar todos usuários cadastrados");
-        System.out.println("3 - Cadastrar nova pergunta no formulário");
-        System.out.println("4 - Deletar pergunta do formulário");
-        System.out.println("5 - Pesquisar usuário por nome, idade ou e-mail");
-        int resposta = sc.nextInt();
-
-        switch (resposta) {
-            case 1: User user2 = coletarDados(sc);
-            salvarDados(user2, path);
-            case 2:
-                listUsers(path);
-            case 3:
-                mainMenu(path2);
-
-
+        try {
+            User user1 = coletarDados(sc);
+            salvarDados(user1, path);
+        } catch (Exception e) {
+            System.out.println("Error in catch void main: " + e.getMessage());
         }
+        mainMenu(path);
+
+        sc.close();
     }
+
     private static List<String> lerPerguntas(String path) {
         List<String> perguntas = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+        try (BufferedReader br = new BufferedReader(new FileReader("E:\\Documents\\diasante\\ws-intellij\\desafio\\sistemas-cadastros\\src\\util\\archives\\mainMenuQuestions.txt"))) {
             String line = br.readLine();
 
             while (line != null) {
@@ -53,17 +39,29 @@ public class Main {
         }
         return perguntas;
     }
-    private static User coletarDados(Scanner sc) {
+
+    private static User coletarDados(Scanner sc) throws Exception{
         System.out.println("#===#Cadastro de Usuário#===#");
+
         System.out.print("Digite o nome do usuário que você gostaria de cadastrar: ");
         String name = sc.nextLine();
+        nameValidator(name);
+
         System.out.print("Digite o e-mail de " + name + ": ");
         String email = sc.nextLine();
+        emailValidator(email);
+        registeredEmails.add(email);
+
         System.out.print("Digite a idade de " + name + ": ");
         Integer age = sc.nextInt();
+        ageValidator(age);
+
         System.out.print("Digite a altura de " + name + ": ");
         Double height = sc.nextDouble();
+        heightValidator(height);
+
         return new User(name, email, age, height);
+
     }
 
     private static String generateArchiveName(String username, String path) {
@@ -96,7 +94,7 @@ public class Main {
 
     private static void salvarDados(User user, String path) {
         String archiveName = generateArchiveName(user.getName().toUpperCase(), path);
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(archiveName))){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(archiveName))) {
             bw.write(user.getName());
             bw.newLine();
             bw.write(user.getEmail());
@@ -112,72 +110,43 @@ public class Main {
         }
     }
 
-    private static void mainMenu (String path) {
+    private static void mainMenu(String path) {
         Scanner sc = new Scanner(System.in);
+        List<String> perguntas = lerPerguntas(path);
+        System.out.println("MENU PRINCIPAL:");
+        perguntas.forEach(System.out::println);
 
-        System.out.println("Digite a pergunta que você gostaria de adicionar: ");
-        String newQuestion = sc.nextLine();
-
-        File file = new File("E:\\Documents\\diasante\\ws-intellij\\desafio\\sistemas-cadastros\\src\\util\\archives\\mainMenuQuestions.txt");
-        File[] files = file.listFiles();
-        int maxNumber = 0;
-        if (files != null) {
-            for (File question : files) {
-                String fileName = question.getName();
-
-                try {
-                    int number = Integer.parseInt(fileName.split("-")[0]);
-                    if (number > maxNumber) {
-                        maxNumber = number;
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println("Error in method mainMenu: " + e.getMessage());
-                }
-            }
-        }
-
-
-        try (BufferedReader br = new BufferedReader(new FileReader("E:\\Documents\\diasante\\ws-intellij\\desafio\\sistemas-cadastros\\src\\util\\archives\\mainMenuQuestions.txt"))){
-            String line = br.readLine();
-
-            while (line != null ){
-                System.out.println(line);
-                line = br.readLine();
-            }
-
-
-        } catch (IOException e) {
-            System.out.println("Error in method mainMenu: " + e.getMessage());
-        }
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("E:\\Documents\\diasante\\ws-intellij\\desafio\\sistemas-cadastros\\src\\util\\archives\\mainMenuQuestions.txt", true))){
-            bw.newLine();
-            bw.write(maxNumber + " - " + newQuestion);
-        } catch (IOException e) {
-            System.out.println("Error in method mainMenu: " + e.getMessage());
-        }
-
-        /*
-        System.out.println("#===#MENU PRINCIPAL#===#");
-        System.out.println("1 - Cadastrar usuário");
-        System.out.println("2 - Listar todos usuários cadastrados");
-        System.out.println("3 - Cadastrar nova pergunta no formulário");
-        System.out.println("4 - Deletar pergunta do formulário");
-        System.out.println("5 - Pesquisar usuário por nome, idade ou e-mail");
         int resposta = sc.nextInt();
+        sc.nextLine();
 
         switch (resposta) {
-            case 1: User user2 = coletarDados(sc);
-                salvarDados(user2, path);
+            case 1:
+                try {
+                    User user = coletarDados(sc);
+                    salvarDados(user, path);
+                } catch (Exception e) {
+                    System.out.println("Error in switch case 1: " + e.getMessage());
+                }
+                break;
             case 2:
                 listUsers(path);
+                break;
             case 3:
-                System.out.println("Digite a pergunta que você gostaria de cadastrar: ");
-                String newQuestion2 = sc.nextLine();
+                newQuestion("E:\\Documents\\diasante\\ws-intellij\\desafio\\sistemas-cadastros\\src\\util\\archives\\mainMenuQuestions.txt");
+                break;
+            case 4:
+                deleteQuestion("E:\\Documents\\diasante\\ws-intellij\\desafio\\sistemas-cadastros\\src\\util\\archives\\mainMenuQuestions.txt");
+                break;
+            case 5:
+                search(path);
+                break;
+            default:
+                System.out.println("xD");
         }
-         */
+
     }
 
-    private static void listUsers (String path) {
+    private static void listUsers(String path) {
         File file = new File(path);
         File[] files = file.listFiles();
 
@@ -185,10 +154,134 @@ public class Main {
             System.out.println("Usuários cadastrados até o momento: ");
             for (File fil : files) {
                 String fileName = fil.getName();
-                if (fileName.matches("^\\d+-.*\\.txt$"));
-                String username = fileName.substring(fileName.indexOf("-") + 1, fileName.lastIndexOf("."));
-                System.out.println(username);
+                if (fileName.matches("^\\d+-.*\\.txt$")) {
+                    String username = fileName.substring(fileName.indexOf("-") + 1, fileName.lastIndexOf("."));
+                    System.out.println(username);
+                }
             }
+        }
+    }
+
+    private static void newQuestion(String path) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a pergunta que você gostaria de adicionar: ");
+        String newQuestion = sc.nextLine();
+        int maxNumber = 0;
+        List<String> perguntas = new ArrayList<>();
+
+        String fileName = path;
+        File file = new File(fileName);
+        System.out.println("Verificando caminho: " + file.getAbsolutePath());
+        System.out.println("Arquivo existe? " + file.exists());
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+
+
+            while (line != null) {
+                perguntas.add(line);
+                if (line.matches("^\\d+ - .*")) {
+                    int number = Integer.parseInt(line.split("\\s+-\\s+")[0]);
+                    maxNumber = Math.max(maxNumber, number);
+                }
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error in method new Question: " + e.getMessage());
+        }
+        String newQuestionFormatted = (maxNumber + 1) + " - " + newQuestion;
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            bw.newLine();
+            bw.write(newQuestionFormatted);
+            bw.flush();
+            System.out.println("Pergunta adicionada com sucesso!");
+        } catch (IOException e) {
+            System.out.println("Error in method newQuestion: " + e.getMessage());
+        }
+    }
+
+    private static void deleteQuestion(String path) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o número da pergunta que você gostaria de deletar (não é possível apagar as 4 primeiras pergunta): ");
+        String numberQuestionDelete = sc.nextLine();
+
+        List<String> perguntas = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+            int lineNumber = 1;
+
+            while (line != null) {
+                if (line.contains(numberQuestionDelete) && lineNumber > 4) {
+                    System.out.println("Pergunta removida: " + line);
+                } else {
+                    perguntas.add(line);
+                }
+                lineNumber++;
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error in method deleteQuestion: " + e.getMessage());
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            for (String pergunta : perguntas) {
+                bw.write(pergunta);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error in method deleteQuestion: " + e.getMessage());
+        }
+    }
+
+    private static void search(String path) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o nome de quem você deseja buscar: ");
+        String searchFor = sc.nextLine().toLowerCase();
+        File file = new File(path);
+        File[] files = file.listFiles();
+
+        if (files != null) {
+            for (File fil : files) {
+                String fileName = fil.getName().toLowerCase();
+                if (fileName.contains(searchFor)) {
+                    String name = fileName.substring(fileName.indexOf("-") + 1, fileName.lastIndexOf("."));
+                    System.out.println(name);
+                }
+            }
+        }
+        sc.close();
+    }
+
+    private static void nameValidator(String name) throws Exception {
+        if (name.length() < 10) {
+            throw new Exception("Nome deve ter pelo menos 10 caracteres.");
+        }
+    }
+
+    private static final Set<String> registeredEmails = new HashSet<>();
+
+    private static void emailValidator(String email) throws Exception {
+        if (!email.contains("@")) {
+            throw new Exception("E-mail deve conter @");
+        }
+        if (registeredEmails.contains(email)) {
+            throw new Exception("E-mail já cadastrado!");
+        }
+    }
+
+    private static void ageValidator(Integer age) throws Exception {
+        if (age < 18) {
+            throw new Exception("Para se cadastrar é necessário ter 18 anos ou mais.");
+        }
+    }
+
+    private static void heightValidator(Double height) throws Exception {
+        String sHeight = Double.toString(height);
+        if (!sHeight.contains(".")) {
+            throw new Exception("Altura inválida. Utilize o ponto (.) ou (,)");
         }
     }
 }
